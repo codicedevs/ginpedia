93cf292a75ba437d03222a5e2d6aab0b23d99c3a
Staladev <siminicnicolas@gmail.com>
2024-09-05 15:56:43 -0300

    added function for title

---
+++ mobile/utils/text.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Div, Text } from 'react-native-magnus';
+import { YellowBorderText } from '../components/styled/styled';
+
+export const TitleGenerator = ({ title }: { title: string }) => {
+    const firstWords = title.slice(0, 2);
+    const restOfText = title.slice(2);
+
+    return (
+        <Div flexDir='row'>
+            <YellowBorderText>
+                <Text
+                    fontSize={'2xl'}
+                >
+                    {firstWords}
+                </Text>
+            </YellowBorderText>
+            <Div>
+                <Text
+                    fontSize={'2xl'}
+                >{restOfText}</Text>
+            </Div>
+        </Div>
+    );
+};
