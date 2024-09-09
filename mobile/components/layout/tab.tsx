e433828de6df74320d47aeac835aeae431f5d15b
Staladev <siminicnicolas@gmail.com>
2024-09-09 15:23:12 -0300

    refactor

--- mobile/components/layout/tab.tsx
+++ mobile/components/layout/tab.tsx
@@ -2,14 +2,13 @@ import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { TouchableOpacity } from "react-native";
 import { Div, Image } from "react-native-magnus";
 import { scale, verticalScale } from "react-native-size-matters";
-import { customTheme } from "../../utils/theme";
 
 const home = require('../../assets/home.png')
 const search = require('../../assets/search.png')
 
 export const MyTab: React.FC<BottomTabBarProps> = ({ state, descriptors, navigation }) => {
     return (
-        <Div style={{ flexDirection: 'row', backgroundColor: customTheme.colors.secondary }}>
+        <Div flexDir="row" bg="secondary">
             {state.routes.map((route, index) => {
                 const { options } = descriptors[route.key];
 
@@ -37,17 +36,18 @@ export const MyTab: React.FC<BottomTabBarProps> = ({ state, descriptors, navigat
                 };
 
                 return (
-                    <TouchableOpacity
-                        accessibilityRole="button"
-                        accessibilityState={isFocused ? { selected: true } : {}}
-                        accessibilityLabel={options.tabBarAccessibilityLabel}
-                        testID={options.tabBarTestID}
-                        onPress={onPress}
-                        onLongPress={onLongPress}
-                        style={{ flex: 1, alignItems: 'center', justifyContent: 'center', height: verticalScale(80) }}
-                    >
-                        <Image w={scale(25)} h={verticalScale(25)} source={label} />
-                    </TouchableOpacity>
+                    <Div flex={1} alignItems="center" justifyContent="center" h={verticalScale(80)}>
+                        <TouchableOpacity
+                            accessibilityRole="button"
+                            accessibilityState={isFocused ? { selected: true } : {}}
+                            accessibilityLabel={options.tabBarAccessibilityLabel}
+                            testID={options.tabBarTestID}
+                            onPress={onPress}
+                            onLongPress={onLongPress}
+                        >
+                            <Image w={scale(25)} h={verticalScale(25)} source={label} />
+                        </TouchableOpacity>
+                    </Div>
                 );
             })}
         </Div>
