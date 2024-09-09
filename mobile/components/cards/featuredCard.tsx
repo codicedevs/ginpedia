80a3f961b957c885fe5c1ef400009caa4694d2cb
Staladev <siminicnicolas@gmail.com>
2024-09-09 15:28:22 -0300

    refactor

--- mobile/components/cards/featuredCard.tsx
+++ mobile/components/cards/featuredCard.tsx
@@ -6,14 +6,14 @@ import { customTheme } from "../../utils/theme"
 export const FeaturedCard = ({ image, title, rating }: { image: string, title: string, rating: string }) => {
     return (
         <Div p={customTheme.spacing.medium} h={verticalScale(170)} w={scale(180)} rounded='xl' flexDir="row" style={{ backgroundColor: "grey" }} >
-            <Div w={'50%'}>
+            <Div flex={1}>
                 <Div flexDir="row">
                     <Icon color="secondary" mr={'md'} name="star" />
                     <Text>{rating}</Text>
                 </Div>
                 <TitleGenerator title={title} size="3xl" />
             </Div>
-            <Div w={'50%'} alignItems="center">
+            <Div flex={1} alignItems="center">
                 <Image resizeMode="contain" style={{ height: verticalScale(150), width: scale(150) }} source={require('../../assets/Bottle.png')} />
             </Div>
         </Div>
