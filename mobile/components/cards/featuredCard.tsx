aca8ced4480f951e22fde978dd3a9550741d0abe
Staladev <siminicnicolas@gmail.com>
2024-09-09 12:53:29 -0300

    new card for home

---
+++ mobile/components/cards/featuredCard.tsx
@@ -0,0 +1,21 @@
+import { Div, Icon, Image, Text } from "react-native-magnus"
+import { scale, verticalScale } from "react-native-size-matters"
+import { TitleGenerator } from "../../utils/text"
+import { customTheme } from "../../utils/theme"
+
+export const FeaturedCard = ({ image, title, rating }: { image: string, title: string, rating: string }) => {
+    return (
+        <Div p={customTheme.spacing.medium} h={verticalScale(170)} w={scale(180)} rounded='xl' flexDir="row" style={{ backgroundColor: "grey" }} >
+            <Div w={'50%'}>
+                <Div flexDir="row">
+                    <Icon color="secondary" mr={'md'} name="star" />
+                    <Text>{rating}</Text>
+                </Div>
+                <TitleGenerator title={title} size="3xl" />
+            </Div>
+            <Div w={'50%'} alignItems="center">
+                <Image resizeMode="contain" style={{ height: verticalScale(150), width: scale(150) }} source={require('../../assets/Bottle.png')} />
+            </Div>
+        </Div>
+    )
+}
