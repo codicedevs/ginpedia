1dd29db644bc290b2fb9ecf9e65a610cdffb692b
Staladev <siminicnicolas@gmail.com>
2024-09-09 15:31:06 -0300

    refactor

--- mobile/components/cards/listCard.tsx
+++ mobile/components/cards/listCard.tsx
@@ -5,14 +5,14 @@ import { TitleGenerator } from "../../utils/text"
 export const ListCard = ({ image, title, rating }: { image: string, title: string, rating: string }) => {
     return (
         <Div p={'xl'} h={verticalScale(100)} w={'100%'} rounded='xl' flexDir="row" style={{ backgroundColor: "grey" }} >
-            <Div w={'70%'}>
+            <Div flex={3}>
                 <Div flexDir="row">
                     <Icon color="secondary" mr={'md'} name="star" />
                     <Text>{rating}</Text>
                 </Div>
                 <TitleGenerator title={title} size="4xl" />
             </Div>
-            <Div justifyContent="center" w={'30%'}>
+            <Div justifyContent="center" flex={1}>
                 <Image resizeMode="contain" style={{ height: verticalScale(80), width: scale(150) }} source={require('../../assets/Bottle.png')} />
             </Div>
         </Div>
