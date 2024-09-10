c29622c9029ea6c31c5c857e548e465855148541
Staladev <siminicnicolas@gmail.com>
2024-09-10 15:17:09 -0300

    list card with skeletton

--- mobile/components/cards/listCard.tsx
+++ mobile/components/cards/listCard.tsx
@@ -1,20 +1,35 @@
-import { Div, Icon, Image, Text } from "react-native-magnus"
-import { scale, verticalScale } from "react-native-size-matters"
-import { TitleGenerator } from "../../utils/text"
+import { Div, Icon, Image, Skeleton, Text } from "react-native-magnus";
+import { scale, verticalScale } from "react-native-size-matters";
+import { TitleGenerator } from "../../utils/text";
 
-export const ListCard = ({ image, title, rating }: { image: string, title: string, rating: string }) => {
+export const ListCard = ({ image, title, rating, isLoading, alreadyFetched }: { image: string, title: string, rating: string, isLoading: boolean, alreadyFetched: boolean }) => {
+    if (isLoading && !alreadyFetched) {
+        return (
+            <Div w={'100%'} mb={'sm'}>
+                <Skeleton
+                    w="100%"
+                    h={verticalScale(100)}
+                    p="xl"
+                    rounded="xl"
+                    flexDir="row"
+                    style={{ backgroundColor: "grey" }}
+                />
+            </Div>
+        );
+    }
+
     return (
-        <Div p={'xl'} h={verticalScale(100)} w={'100%'} rounded='xl' flexDir="row" style={{ backgroundColor: "grey" }} >
-            <Div flex={3}>
+        <Div p={'xl'} h={verticalScale(100)} w={'100%'} rounded='xl' flexDir="row" style={{ backgroundColor: "grey" }} mb={10} >
+            <Div flex={6}>
                 <Div flexDir="row">
                     <Icon color="secondary" mr={'md'} name="star" />
                     <Text>{rating}</Text>
                 </Div>
                 <TitleGenerator title={title} size="4xl" />
             </Div>
-            <Div justifyContent="center" flex={1}>
+            <Div justifyContent="center" alignItems="center" flex={1}>
                 <Image resizeMode="contain" style={{ height: verticalScale(80), width: scale(150) }} source={require('../../assets/Bottle.png')} />
             </Div>
         </Div>
-    )
+    );
 }
