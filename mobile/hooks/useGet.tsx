51b419eeb006383bb74a9778d8433f779a36632b
Staladev <siminicnicolas@gmail.com>
2024-09-10 15:22:58 -0300

    added initial data for skeletton

--- mobile/hooks/useGet.tsx
+++ mobile/hooks/useGet.tsx
@@ -6,6 +6,23 @@ function useFetch<T>(fn: FetchFunction<T>, key: string, options = {}) {
     return useQuery<T[]>({
         queryKey: [key],
         queryFn: fn,
+        initialData: [
+            {
+                id: 1,
+                title: "Initial Item 1",
+                completed: false
+            },
+            {
+                id: 2,
+                title: "Initial Item 2",
+                completed: false
+            },
+            {
+                id: 3,
+                title: "Initial Item 3",
+                completed: true
+            }
+        ] as T[],
         ...options,
         meta: {
             triggerGlobalLoader: false, // Activar por defecto
