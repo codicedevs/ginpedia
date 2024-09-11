7dad0c3080e63b5c1ce9d955cac64acd3d1631be
Staladev <siminicnicolas@gmail.com>
2024-09-11 17:27:20 -0300

    change in list card

--- mobile/components/cards/listCard.tsx
+++ mobile/components/cards/listCard.tsx
@@ -1,8 +1,9 @@
 import { Div, Icon, Image, Skeleton, Text } from "react-native-magnus";
 import { scale, verticalScale } from "react-native-size-matters";
 import { TitleGenerator } from "../../utils/text";
+import { customTheme } from "../../utils/theme";
 
-export const ListCard = ({ image, title, rating, isLoading, alreadyFetched }: CardProps) => {
+export const ListCard = ({ image, title, rating, isLoading, alreadyFetched, punctuation }: CardProps) => {
     if (isLoading && !alreadyFetched) {
         return (
             <Div w={'100%'} mb={'sm'}>
@@ -19,13 +20,17 @@ export const ListCard = ({ image, title, rating, isLoading, alreadyFetched }: Ca
     }
 
     return (
-        <Div p={'xl'} h={verticalScale(100)} w={'100%'} rounded='xl' flexDir="row" bg="grey" mb={10} >
+        <Div p={'xl'} h={verticalScale(100)} w={'100%'} rounded='xl' flexDir="row" bg="grey" mb={'lg'} >
             <Div flex={6}>
                 <Div flexDir="row">
                     <Icon color="secondary" mr={'md'} name="star" />
                     <Text>{rating}</Text>
                 </Div>
                 <TitleGenerator title={title} size="4xl" />
+                {
+                    !!punctuation &&
+                    <Text mt={'sm'} fontSize={customTheme.fontSize.small}>{punctuation} Puntuaciones</Text>
+                }
             </Div>
             <Div justifyContent="center" alignItems="center" flex={1}>
                 <Image resizeMode="contain" style={{ height: verticalScale(80), width: scale(150) }} source={require('../../assets/Bottle.png')} />
