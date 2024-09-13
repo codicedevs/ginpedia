9a433be2ec94792545eadbb07c8dd9e62635ec47
Staladev <siminicnicolas@gmail.com>
2024-09-13 11:44:45 -0300

    fix in title util

--- mobile/utils/text.tsx
+++ mobile/utils/text.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import { Div } from 'react-native-magnus';
+import { Div, Text } from 'react-native-magnus';
 import { TitleText, YellowBorderText } from '../components/styled/styled';
 
 export const TitleGenerator = ({ title, size = '2xl' }: { title: string, size?: string }) => {
-    const firstWords = title.slice(0, 2);
-    const restOfText = title.slice(2);
+    console.log(title, 'q pasa');
+
+    if (!title) {
+        return (
+            <Text>Error</Text>
+        );
+    }
+
+    // Dividimos el texto por palabras, obtenemos la primera palabra y el resto
+    const words = title.split(' ');
+    const firstWord = words[0]; // Primera palabra
+    const restOfText = words.slice(1).join(' '); // El resto del texto
+
+    // Dividimos la primera palabra en las primeras 2 letras y el resto
+    const firstTwoLetters = firstWord.slice(0, 2);
+    const restOfFirstWord = firstWord.slice(2);
 
     return (
-        <Div flexDir='row'>
-            <YellowBorderText>
+        <Div flexDir='row' flexWrap='wrap'>
+            <Div flexDir='row'>
+                <YellowBorderText>
+                    <TitleText
+                        fontSize={size}
+                        maxW={'100%'}
+                    >
+                        {firstTwoLetters}
+                    </TitleText>
+                </YellowBorderText>
                 <TitleText
                     fontSize={size}
+                    maxW={'100%'}
                 >
-                    {firstWords}
+                    {`${restOfFirstWord} `}
                 </TitleText>
-            </YellowBorderText>
+            </Div>
             <TitleText
                 fontSize={size}
-            >{restOfText}</TitleText>
+                maxW={'100%'}
+            >
+                {restOfText}
+            </TitleText>
         </Div>
     );
 };
