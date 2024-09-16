24cfa1dd074227868aa1bdbe6b9b4e55c7b2adab
Staladev <siminicnicolas@gmail.com>
2024-09-16 12:24:25 -0300

    change skeletton

--- mobile/components/cards/featuredCard.tsx
+++ mobile/components/cards/featuredCard.tsx
@@ -1,17 +1,20 @@
-import { Div, Icon, Image, Skeleton, Text } from "react-native-magnus"
+import { Div, Icon, Image, Text } from "react-native-magnus"
 import { scale, verticalScale } from "react-native-size-matters"
 import { TitleGenerator } from "../../utils/text"
 import { customTheme } from "../../utils/theme"
+import { FeaturedCardSkeletton } from "../styled/styled"
 
 export const FeaturedCard = ({ image, title, rating, isLoading, alreadyFetched }: CardProps) => {
     if (isLoading && !alreadyFetched) {
         return (
             <Div h={verticalScale(170)} w={scale(180)}>
-                <Skeleton
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundColor: 'gray'
+                <FeaturedCardSkeletton
+                    from={{ opacity: 0.5 }}
+                    animate={{ opacity: 1 }}
+                    transition={{
+                        type: 'timing',
+                        duration: 1000,
+                        loop: true,
                     }}
                 />
             </Div>
