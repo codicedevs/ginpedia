4aba193a00b4e0f9710112a8411d00045d4801e4
Staladev <siminicnicolas@gmail.com>
2024-09-17 17:13:02 -0300

    change in header

--- mobile/components/layout/header.tsx
+++ mobile/components/layout/header.tsx
@@ -1,10 +1,11 @@
-import { Text } from "react-native"
+
 import { Div } from "react-native-magnus"
+import { TitleText } from "../styled/styled"
 
 export const MyHeader = () => {
     return (
         <Div flexDir='row' justifyContent='space-between' alignItems='center' py={'lg'} mb={'sm'} >
-            <Text>Ginpedia</Text>
+            <TitleText>Ginpedia</TitleText>
             <Div h={50} w={50} bg='black' rounded={'circle'}></Div>
         </Div>
     )
