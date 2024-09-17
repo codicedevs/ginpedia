2c9ff5a6072f1cc211121429e89c119d6dadfa84
Staladev <siminicnicolas@gmail.com>
2024-09-17 17:13:13 -0300

    filter

--- mobile/components/List/listFilterSelector.tsx
+++ mobile/components/List/listFilterSelector.tsx
@@ -16,35 +16,38 @@ export const ListFilterSelector = ({ handler, value, openSelect, currentFilter }
         <Div h={verticalScale(35)} w={'100%'} flexDir='row'>
             <Button
                 onPress={() => handler(FilterOptions.GIN)}
-                bg={FilterOptions.GIN === value ? 'secondary' : 'grey'}
+                bg={FilterOptions.GIN === value ? 'secondary' : 'background'}
                 flex={1}
                 justifyContent="center"
                 alignItems="center"
                 rounded="xl"
+                mr={4}
             >
                 <Text color={FilterOptions.GIN === value ? 'black' : 'white'}>Gin</Text>
             </Button>
             <Button
                 onPress={() => handler(FilterOptions.ESPECIA)}
-                bg={FilterOptions.ESPECIA === value ? 'secondary' : 'grey'}
+                bg={FilterOptions.ESPECIA === value ? 'secondary' : 'background'}
                 flex={1}
                 justifyContent="center"
                 alignItems="center"
                 rounded="xl"
+                mr={4}
             >
                 <Text color={FilterOptions.ESPECIA === value ? 'black' : 'white'}>Especia</Text>
             </Button>
             <Button
                 onPress={() => handler(FilterOptions.TONICA)}
-                bg={FilterOptions.TONICA === value ? 'secondary' : 'grey'}
+                bg={FilterOptions.TONICA === value ? 'secondary' : 'background'}
                 flex={1}
                 justifyContent="center"
                 alignItems="center"
                 rounded="xl"
+                mr={4}
             >
                 <Text color={FilterOptions.TONICA === value ? 'black' : 'white'}>Tonica</Text>
             </Button>
-            <TouchableImageFilter onPress={openSelect} style={{ backgroundColor: currentFilter.id !== '1' ? customTheme.colors.secondary : 'grey' }}>
+            <TouchableImageFilter onPress={openSelect} style={{ backgroundColor: currentFilter.id !== '1' ? customTheme.colors.secondary : customTheme.colors.background }}>
                 <Image resizeMode='contain' h={'50%'} w={'50%'} source={require('../../assets/filterSelector.png')} />
             </TouchableImageFilter>
         </Div>
