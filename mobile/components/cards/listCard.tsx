1a622cd215b68f7065987b6ec01b0e9f3e268919
Staladev <siminicnicolas@gmail.com>
2024-09-20 11:20:22 -0300

    changes in card, now can see detail

--- mobile/components/cards/listCard.tsx
+++ mobile/components/cards/listCard.tsx
@@ -1,10 +1,23 @@
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { TouchableOpacity } from "react-native";
 import { Div, Icon, Image, Text } from "react-native-magnus";
 import { scale, verticalScale } from "react-native-size-matters";
+import { AppScreens, AppScreensParamList } from "../../navigation/screens";
 import { TitleGenerator } from "../../utils/text";
 import { customTheme } from "../../utils/theme";
 import { ListCardSkeletton } from "../styled/styled";
+import { CardProps } from "./card.types";
 
-export const ListCard = ({ image, title, rating, isLoading, alreadyFetched, punctuation }: CardProps) => {
+type NavigationProps = NativeStackNavigationProp<AppScreensParamList, AppScreens.HOME_SCREEN>;
+
+export const ListCard = ({ product, isLoading, alreadyFetched }: CardProps) => {
+    const navigation = useNavigation<NavigationProps>();
+
+    const navigateToDetail = () => {
+        navigation.navigate(AppScreens.PRODUCT_DETAIL_SCREEN, { productId: product.id });
+    }
+
     if (isLoading && !alreadyFetched) {
         return (
             <Div w={'100%'} mb={'sm'}>
@@ -21,22 +34,26 @@ export const ListCard = ({ image, title, rating, isLoading, alreadyFetched, punc
         );
     }
 
+    console.log(product)
+
     return (
-        <Div p={'xl'} h={verticalScale(100)} w={'100%'} rounded='xl' flexDir="row" bg="cardBg" mb={'lg'} >
-            <Div flex={6}>
-                <Div flexDir="row">
-                    <Icon color="secondary" mr={'md'} name="star" />
-                    <Text color="black">{rating}</Text>
+        <TouchableOpacity onPress={navigateToDetail} >
+            <Div p={'xl'} h={verticalScale(100)} w={'100%'} rounded='xl' flexDir="row" bg="cardBg" mb={'lg'} >
+                <Div flex={6}>
+                    <Div flexDir="row">
+                        <Icon color="secondary" mr={'md'} name="star" />
+                        <Text color="black">{product.rating ? product.rating : 'Nadie rateo esto'}</Text>
+                    </Div>
+                    <TitleGenerator color="black" title={product.name} size="4xl" />
+                    {
+                        !!product.punctiation &&
+                        <Text color="black" mt={'sm'} fontSize={customTheme.fontSize.small}>{product.punctiation} Puntuaciones</Text>
+                    }
                 </Div>
-                <TitleGenerator color="black" title={title} size="4xl" />
-                {
-                    !!punctuation &&
-                    <Text color="black" mt={'sm'} fontSize={customTheme.fontSize.small}>{punctuation} Puntuaciones</Text>
-                }
+                <Div justifyContent="center" alignItems="center" flex={1}>
+                    <Image resizeMode="contain" style={{ height: verticalScale(80), width: scale(150) }} source={require('../../assets/Bottle.png')} />
+                </Div>
             </Div>
-            <Div justifyContent="center" alignItems="center" flex={1}>
-                <Image resizeMode="contain" style={{ height: verticalScale(80), width: scale(150) }} source={require('../../assets/Bottle.png')} />
-            </Div>
-        </Div>
+        </TouchableOpacity>
     );
 }
