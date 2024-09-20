be81cb7c2b5c0fd192b32ac32dbcf3cdf893f249
Staladev <siminicnicolas@gmail.com>
2024-09-20 16:29:39 -0300

    showing actual data

--- mobile/screens/productDetail.tsx
+++ mobile/screens/productDetail.tsx
@@ -1,27 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, TouchableOpacity } from 'react-native';
 import { Button, Div, Icon, Image, Overlay, ScrollDiv, Text } from 'react-native-magnus';
 import Carousel from 'react-native-reanimated-carousel';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { scale, verticalScale } from 'react-native-size-matters';
 import { MyHeader } from '../components/layout/header';
 import { BoldText, InfoContainer, RatingModalInfo } from '../components/styled/styled';
+import useFetch from '../hooks/useGet';
 import { AppScreenProps, AppScreens } from '../navigation/screens';
+import productService from '../service/product.service';
+import { Product } from '../types/product.type';
+import { BASE_URL } from '../utils/config';
 import { TitleGenerator } from '../utils/text';
 import { customTheme } from '../utils/theme';
 
 function ProductDetail({ route, navigation }: AppScreenProps<AppScreens.PRODUCT_DETAIL_SCREEN>) {
     const width = Dimensions.get('window').width;
     const [currentIndex, setCurrentIndex] = useState(0);
-    const data = [...new Array(6).keys()];
+    const info = [...new Array(6).keys()];
     const [open, setOpen] = useState(false)
     const { productId } = route.params;
+
+    const fetchProduct = async () => {
+        if (!productId) return
+        const res = await productService.getById(productId)
+        return res
+    }
+
+    const { data: product, isFetching, isFetched } = useFetch<Product>(fetchProduct, ['products', productId]);
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <StatusBar style='auto' />
-
             <Overlay w={scale(310)} h={verticalScale(363)} bg='#D9D9D9' visible={open} p="xl">
                 <Div justifyContent='space-between' h={'100%'}>
                     <Div>
@@ -83,18 +94,19 @@ function ProductDetail({ route, navigation }: AppScreenProps<AppScreens.PRODUCT_
                     </Div>
                 </Div>
             </Overlay>
-
             <Div bg='background' flex={1} px={'xl'}>
                 <ScrollDiv showsVerticalScrollIndicator={false} flex={1}>
                     <MyHeader />
                     <Div mb={'xl'} alignItems='flex-start'>
-                        <Icon name='arrowleft' fontSize={'3xl'} color='secondary' />
+                        <TouchableOpacity onPress={() => navigation.goBack()}>
+                            <Icon name='arrowleft' fontSize={'3xl'} color='secondary' />
+                        </TouchableOpacity>
                     </Div>
                     <Carousel
                         loop
                         width={scale(300)}
                         height={verticalScale(211)}
-                        data={data}
+                        data={info}
                         scrollAnimationDuration={1000}
                         onProgressChange={(_, absoluteProgress) => {
                             setCurrentIndex(Math.round(absoluteProgress));
@@ -105,14 +117,12 @@ function ProductDetail({ route, navigation }: AppScreenProps<AppScreens.PRODUCT_
                                 borderWidth={scale(1)}
                                 justifyContent='center'
                             >
-                                <Text textAlign='center' fontSize={'xl'}>
-                                    {index}
-                                </Text>
+                                <Image resizeMode="contain" style={{ height: '100%', width: '100%' }} source={{ uri: `${BASE_URL}/${product?.image}` }} />
                             </Div>
                         )}
                     />
                     <Div flexDir='row' justifyContent='center' mt={'lg'}>
-                        {data.map((_, index) => (
+                        {info.map((_, index) => (
                             <Div
                                 w={scale(10)}
                                 h={verticalScale(10)}
@@ -134,20 +144,20 @@ function ProductDetail({ route, navigation }: AppScreenProps<AppScreens.PRODUCT_
                         <Icon color="secondary" fontSize='2xl' name="heart" />
                     </Div>
                     <Div mb={'xl'}>
-                        <TitleGenerator title="Nombre del producto" />
+                        <TitleGenerator title={product?.name} />
                     </Div>
                     <Text mb={'lg'} textAlign='justify'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu commodo enim. Maecenas condimentum dolor a est varius, eu efficitur enim blandit. Nunc interdum sem vitae pulvinar feugiat. Nullam rutrum, lacus at vulputate lacinia, purus diam vestibulum mauris, eu fermentum mi magna vitae est. Nam molestie condimentum efficitur. Nulla aliquet mauris a condimentum mattis. Ut quis elit vitae dolor interdum fermentum vel quis diam.
+                        {product?.description}
                     </Text>
                     <InfoContainer>
                         <BoldText>Informacion del producto</BoldText>
                         <Div flexDir='row'>
                             <Icon mr={'sm'} fontFamily='Entypo' color="secondary" fontSize='2xl' name="location-pin" />
-                            <Text>PremiumDrink,STOCK,Sweden</Text>
+                            <Text>{product?.origin}</Text>
                         </Div>
                         <Div flexDir='row'>
                             <Icon mr={'sm'} fontFamily='Feather' color="secondary" fontSize='2xl' name="percent" />
-                            <Text>37,5</Text>
+                            <Text>{product?.graduation}</Text>
                         </Div>
                     </InfoContainer>
 
@@ -166,11 +176,11 @@ function ProductDetail({ route, navigation }: AppScreenProps<AppScreens.PRODUCT_
                     <InfoContainer>
                         <BoldText>Aderezos perfectos para esta bebida</BoldText>
                         <Div flexDir='row'>
-                            <Image resizeMode='center' w={scale(10)} h={'auto'} source={require('../assets/icons/orange.png')} />
+                            <Image mr={'sm'} resizeMode='center' w={scale(10)} h={'auto'} source={require('../assets/icons/orange.png')} />
                             <Text>Menta</Text>
                         </Div>
                         <Div flexDir='row'>
-                            <Image resizeMode='center' w={scale(10)} h={'auto'} source={require('../assets/icons/orange.png')} />
+                            <Image mr={'sm'} resizeMode='center' w={scale(10)} h={'auto'} source={require('../assets/icons/orange.png')} />
                             <Text>Limon</Text>
                         </Div>
                     </InfoContainer>
