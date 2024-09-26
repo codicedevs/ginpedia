6b78cf33fbe3ed7b08c4e420f7eb80de23914a59
Matías Trovant <mtrovant@gmail.com>
2024-09-26 14:35:15 -0300

    Cambios para mostrar combinaciones

--- mobile/service/product.service.ts
+++ mobile/service/product.service.ts
@@ -1,21 +1,21 @@
-
 import { FilterParamsProps } from "../types/query.types";
 import { HttpService } from "./http.service";
 
 class ProductService extends HttpService {
-    constructor() {
-        super("products");
-    }
+  constructor() {
+    super("products");
+  }
 
-    getAll = async (filter?: FilterParamsProps) => {
-        const res = await this.get(``, { params: filter });
-        return res.data;
-    };
+  getAll = async (filter?: FilterParamsProps) => {
+    const res = await this.get(``, { params: filter });
+    return res.data;
+  };
 
-    getById = async (id: string) => {
-        const res = await this.get(`/${id}`)
-        return res.data
-    }
+  getById = async (id: string) => {
+    const res = await this.get(`/${id}?withCombination=true`);
+
+    return res.data;
+  };
 }
 
 export default new ProductService();
--- mobile/types/product.type.ts
+++ mobile/types/product.type.ts
@@ -1,18 +1,18 @@
 export enum ProductType {
-    GIN = "gin",
-    TONICA = "tonica",
-    ESPECIA = "especia",
+  GIN = "gin",
+  TONICA = "tonica",
+  ESPECIA = "especia",
 }
 
 export type Product = {
-    name: string;
-    description: string;
-    type: ProductType;
-    image: string;
-    origin: string;
-    graduation: string;
-    combinations?: number[];
-    rating: string;
-    punctuation?: string;
-    id: string
+  name: string;
+  description: string;
+  type: ProductType;
+  image: string;
+  origin: string;
+  graduation: string;
+  combinations?: any[];
+  rating: string;
+  punctuation?: string;
+  id: string;
 };
