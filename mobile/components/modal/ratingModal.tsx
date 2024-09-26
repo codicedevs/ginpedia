956336553e780c413c8cfd0ecb4beebb79df122e
Staladev <siminicnicolas@gmail.com>
2024-09-26 15:59:03 -0300

    modal now sends and shows rating, missing update

--- mobile/components/modal/ratingModal.tsx
+++ mobile/components/modal/ratingModal.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { Button, Div, Icon, Overlay, Text } from "react-native-magnus";
 import { scale, verticalScale } from 'react-native-size-matters';
+import { AuthContext } from '../../context/authProvider';
+import ratingService from '../../service/rating.service';
 import { Product } from '../../types/product.type';
 import { TitleGenerator } from '../../utils/text';
 import { BoldText, RatingModalInfo } from '../styled/styled';
@@ -10,32 +12,35 @@ interface RatingModalProps {
     isVisible: boolean;
     setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
     rating: Product['rating'];
-    ratings: Product['ratingList']
+    ratings: Product['ratingList'];
+    productId: string
 }
-const RatingModal: React.FC<RatingModalProps> = ({ isVisible, setIsVisible, rating, ratings }) => {
+const RatingModal: React.FC<RatingModalProps> = ({ isVisible, setIsVisible, rating, ratings, productId }) => {
+    const { currentUser } = useContext(AuthContext)
+    const [value, setValue] = useState(rating ? rating : 0)
     const totalRatings = ratings.reduce((sum, rating) => {
         const values = Object.values(rating);
         return sum + values[0];
     }, 0);
-    const [value, setValue] = useState(0)
 
     const checkRating = (option: number): number => {
         if (totalRatings === 0) return 0;
-
         const ratingForOption = ratings.find(rating => Object.keys(rating)[0] === option.toString());
-
         if (!ratingForOption) return 0;
 
         const valueForOption = Object.values(ratingForOption)[0] as number;
-
         const percentage = (valueForOption / totalRatings) * 100;
 
         return percentage;
     };
 
+    const sendRating = () => {
+        if (value === 0) return
+        ratingService.createRating(productId, currentUser.id, Number(value))
+    }
+
     const onCancel = () => {
         setIsVisible(false)
-        setValue(0)
     }
 
     return (
@@ -53,37 +58,37 @@ const RatingModal: React.FC<RatingModalProps> = ({ isVisible, setIsVisible, rati
                     <RatingModalInfo>
                         <Text color='black'>5 estrellas</Text>
                         <Div rounded={'md'} w={scale(160)} h={verticalScale(10)} bg='white' >
+                            <Div rounded={'md'} bg='secondary' h={'100%'} w={`${checkRating(5)}%`} />
+                        </Div>
+                        <Text color='black'>{checkRating(5)}%</Text>
+                    </RatingModalInfo>
+                    <RatingModalInfo>
+                        <Text color='black'>4 estrellas</Text>
+                        <Div rounded={'md'} w={scale(160)} h={verticalScale(10)} bg='white' >
                             <Div rounded={'md'} bg='secondary' h={'100%'} w={`${checkRating(4)}%`} />
                         </Div>
                         <Text color='black'>{checkRating(4)}%</Text>
                     </RatingModalInfo>
                     <RatingModalInfo>
-                        <Text color='black'>4 estrellas</Text>
+                        <Text color='black'>3 estrellas</Text>
                         <Div rounded={'md'} w={scale(160)} h={verticalScale(10)} bg='white' >
                             <Div rounded={'md'} bg='secondary' h={'100%'} w={`${checkRating(3)}%`} />
                         </Div>
                         <Text color='black'>{checkRating(3)}%</Text>
                     </RatingModalInfo>
                     <RatingModalInfo>
-                        <Text color='black'>3 estrellas</Text>
+                        <Text color='black'>2 estrellas</Text>
                         <Div rounded={'md'} w={scale(160)} h={verticalScale(10)} bg='white' >
                             <Div rounded={'md'} bg='secondary' h={'100%'} w={`${checkRating(2)}%`} />
                         </Div>
                         <Text color='black'>{checkRating(2)}%</Text>
                     </RatingModalInfo>
                     <RatingModalInfo>
-                        <Text color='black'>2 estrellas</Text>
+                        <Text color='black'>1 estrellas</Text>
                         <Div rounded={'md'} w={scale(160)} h={verticalScale(10)} bg='white' >
                             <Div rounded={'md'} bg='secondary' h={'100%'} w={`${checkRating(1)}%`} />
                         </Div>
                         <Text color='black'>{checkRating(1)}%</Text>
-                    </RatingModalInfo>
-                    <RatingModalInfo>
-                        <Text color='black'>1 estrellas</Text>
-                        <Div rounded={'md'} w={scale(160)} h={verticalScale(10)} bg='white' >
-                            <Div rounded={'md'} bg='secondary' h={'100%'} w={`${checkRating(0)}%`} />
-                        </Div>
-                        <Text color='black'>{checkRating(0)}%</Text>
                     </RatingModalInfo>
                 </Div>
                 <Div flexDir='row' justifyContent='space-between' w={'70%'} alignSelf='center'>
@@ -105,7 +110,7 @@ const RatingModal: React.FC<RatingModalProps> = ({ isVisible, setIsVisible, rati
                 </Div>
                 <Div h={verticalScale(40)} w={'100%'} justifyContent='space-between' flexDir='row' >
                     <Button onPress={onCancel} bg='#BEBEBE' w={scale(120)}><BoldText color='black'>CANCELAR</BoldText></Button>
-                    <Button w={scale(120)} bg='secondary' color='black'><BoldText color='black'>ENVIAR</BoldText></Button>
+                    <Button w={scale(120)} onPress={() => sendRating()} bg='secondary' color='black'><BoldText color='black'>ENVIAR</BoldText></Button>
                 </Div>
             </Div>
         </Overlay>
