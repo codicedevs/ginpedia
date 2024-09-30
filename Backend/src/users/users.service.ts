64e6fe554dd9eb92d636bf8e90b9e076b54ac826
Matías Trovant <mtrovant@gmail.com>
2024-09-30 12:23:13 -0300

    Cambios para stala

--- Backend/src/users/users.service.ts
+++ Backend/src/users/users.service.ts
@@ -157,4 +157,13 @@ export class UsersService {
         : "Este usuario no tiene bookmarks",
     };
   }
+
+  async getUserBookmarks(id: number) {
+    const user = await this.userRepository.findOneOrFail({
+      where: { id: id },
+      relations: ["bookmarks"],
+    });
+    console.log(user);
+    return user.bookmarks;
+  }
 }
