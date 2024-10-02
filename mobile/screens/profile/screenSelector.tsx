9b5ae66febf8e9e778f113ccd52fbf69f247a5ab
Staladev <siminicnicolas@gmail.com>
2024-10-02 11:56:59 -0300

    added logic to move to title

--- mobile/screens/profile/screenSelector.tsx
+++ mobile/screens/profile/screenSelector.tsx
@@ -1,5 +1,5 @@
-import { TouchableOpacity } from "react-native"
-import { ScrollDiv } from "react-native-magnus"
+import { useEffect, useRef } from "react"
+import { ScrollView, TouchableOpacity } from "react-native"
 import { scale } from "react-native-size-matters"
 import { ProfileOption } from "../../types/user.type"
 import { TitleGenerator } from "../../utils/text"
@@ -10,9 +10,42 @@ type SelectorProps = {
 }
 
 const ScreenSelector = ({ option, setOption }: SelectorProps) => {
+    const scrollRef = useRef<ScrollView>(null);
+
+    const scrollToOption = () => {
+        let x = 0;
+        switch (option) {
+            case ProfileOption.PROFILE:
+                x = 0;
+                break;
+            case ProfileOption.WISHLIST:
+                x = scale(80);
+                break;
+            case ProfileOption.STORE:
+                x = scale(180);
+                break;
+            case ProfileOption.PUNCTUATION:
+                x = scale(280);
+                break;
+            default:
+                x = 0;
+        }
+        scrollRef.current?.scrollTo({ x: x, animated: true });
+    };
+
+    useEffect(() => {
+
+        scrollToOption();
+    }, [option]);
+
     return (
         <>
-            <ScrollDiv showsHorizontalScrollIndicator={false} mb={'xl'} flexDir='row' contentContainerStyle={{ gap: scale(17) }} horizontal>
+            <ScrollView
+                ref={scrollRef}
+                showsHorizontalScrollIndicator={false}
+                style={{ marginBottom: 20 }}
+                contentContainerStyle={{ gap: scale(17), width: scale(580) }}
+                horizontal>
                 <TouchableOpacity onPress={() => setOption(ProfileOption.PROFILE)}>
                     <TitleGenerator color={option === ProfileOption.PROFILE ? 'white' : 'grey'} borderColor={option === ProfileOption.PROFILE ? 'secondary' : 'grey'} title='Mi perfil' />
                 </TouchableOpacity>
@@ -28,7 +61,7 @@ const ScreenSelector = ({ option, setOption }: SelectorProps) => {
                 <TouchableOpacity onPress={() => setOption(ProfileOption.PUNCTUATION)}>
                     <TitleGenerator color={option === ProfileOption.PUNCTUATION ? 'white' : 'grey'} borderColor={option === ProfileOption.PUNCTUATION ? 'secondary' : 'grey'} title='Mi puntuacion' />
                 </TouchableOpacity>
-            </ScrollDiv>
+            </ScrollView>
         </>
     )
 }
