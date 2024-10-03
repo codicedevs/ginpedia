e4c6660d314bc68f3e2e3b1dc4dcde33a1df8d2e
Staladev <siminicnicolas@gmail.com>
2024-10-03 11:52:49 -0300

    generate data

--- mobile/screens/profile/profile.tsx
+++ mobile/screens/profile/profile.tsx
@@ -1,16 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Div, ScrollDiv } from 'react-native-magnus';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { MyHeader } from '../../components/layout/header';
+import { BookmarkContext } from '../../context/bookmarkProvider';
+import useFetch from '../../hooks/useGet';
 import { AppScreenProps, AppScreens } from '../../navigation/screens';
-import { ProfileOption } from '../../types/user.type';
+import productService from '../../service/product.service';
+import { Product } from '../../types/product.type';
+import { QUERY_KEYS } from '../../types/query.types';
+import { Bookmark, BookmarkType, ProfileOption } from '../../types/user.type';
 import ProductList from './productList';
 import ProfileInfo from './profileInfo';
 import ScreenSelector from './screenSelector';
 
 function ProfileScreen({ navigation }: AppScreenProps<AppScreens.PROFILE_SCREEN>) {
     const [option, setOption] = useState(ProfileOption.PROFILE)
+    const { bookmarks } = useContext(BookmarkContext)
+    const WISHLIST = bookmarks.filter((bookmark: Bookmark) => bookmark.type === BookmarkType.WISHLIST)
+    const PURCHASED = bookmarks.filter((bookmark: Bookmark) => bookmark.type === BookmarkType.PURCHASED)
+
+    const bringProducts = async () => {
+        try {
+            const res = await productService.getAll();
+            return res;
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    };
+    const { data, isFetching, isFetched } = useFetch<Product[]>(bringProducts, [QUERY_KEYS.PRODUCTS]);
+
+    const filteredWishlistProducts = data.filter(product =>
+        WISHLIST.some(item => item.productId === Number(product.id))
+    );
+
+    const filteredPurchasedProducts = data.filter(product =>
+        PURCHASED.some(item => item.productId === Number(product.id))
+    );
+
+    const setFilter = () => {
+        switch (option) {
+            case ProfileOption.WISHLIST:
+                return filteredWishlistProducts
+            case ProfileOption.PUNCTUATION:
+                return []
+            case ProfileOption.STORE:
+                return filteredPurchasedProducts
+            default:
+                return []
+        }
+    }
 
     return (
         <>
@@ -27,7 +67,7 @@ function ProfileScreen({ navigation }: AppScreenProps<AppScreens.PROFILE_SCREEN>
                         option === ProfileOption.PROFILE ?
                             <ProfileInfo />
                             :
-                            <ProductList />
+                            <ProductList data={setFilter()} />
                     }
                 </Div>
             </SafeAreaView>
