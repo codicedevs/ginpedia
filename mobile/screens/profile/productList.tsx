eef96f7664c2f2d34e69d62f2a027499b3707c49
Staladev <siminicnicolas@gmail.com>
2024-10-03 11:52:58 -0300

    implement

--- mobile/screens/profile/productList.tsx
+++ mobile/screens/profile/productList.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { ListCard } from '../../components/cards/listCard';
-import useFetch from '../../hooks/useGet';
 import productService from '../../service/product.service';
 import { Product } from '../../types/product.type';
 
-function ProductList() {
+function ProductList({ data }: { data: Product[] }) {
     const bringProducts = async () => {
         const res = await productService.getAll()
         return res;
     };
 
-    const { data: products, isFetching, isFetched } = useFetch<Product[]>(bringProducts, ['productsList']);
     return (
         <>
             {
-                products &&
-                products.map((product) => (
-                    <ListCard product={product} alreadyFetched={isFetched} isLoading={isFetching} />
+                data &&
+                data.map((product) => (
+                    <ListCard product={product} alreadyFetched={false} isLoading={false} />
                 ))
             }
         </>
