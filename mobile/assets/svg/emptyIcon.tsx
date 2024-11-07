e1ae3575a539f04059fd58296d2c718af14f5c09
StalaDev <siminicnicolas@gmail.com>
2024-11-07 16:56:11 -0300

    changes in svg

--- mobile/assets/svg/emptyIcon.tsx
+++ mobile/assets/svg/emptyIcon.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Svg, { Path } from 'react-native-svg';
+import { customTheme } from '../../utils/theme';
 
 export const EmptyIcon = () => (
     <Svg
         width="70"
         height="70"
         viewBox="0 0 1024 1024"
-        fill="white"
+        fill={customTheme.colors.secondary}
     >
         <Path d="M839.2 101.3H184.9L65.3 539.5 64 922.7h896V549.3l-120.8-448zM241.9 176h540.3L884 549.3H678.7l-74.7 112H420l-74.7-112H140.1L241.9 176z" />
     </Svg>
