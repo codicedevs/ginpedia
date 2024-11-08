48d8a056ff84666c58f725c783a37fb31aa63ebe
StalaDev <siminicnicolas@gmail.com>
2024-11-08 10:55:52 -0300

    cambio imagen

--- mobile/utils/config.ts
+++ mobile/utils/config.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = "https://www.codice.dev:3020"
+export const BASE_URL = "http://192.168.1.8:3000"
+// export const BASE_URL = "https://www.codice.dev:3020"
 
 export const VERSION = '1.0'
